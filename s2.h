60f18d0b43b543ec6759edaa161fd3e088877e8c
Alex Field <alexfield290@gmail.com>
2021-10-12 17:38:27 +1100

    Files
    Posted all needed files

---
+++ s2.h
@@ -0,0 +1,7 @@
+comp_z80_size 0xF21 e für C-Programm */
+#define word_728C_user 0x751C
+#define Obj5F_MapUnc_7240 0x75A6
+#define off_3A294 0x3EA6A
+#define MapRUnc_Sonic 0xB187E
+#define movewZ80CompSize 0x15604E
+/* Ende Includefile für C-Programm */
